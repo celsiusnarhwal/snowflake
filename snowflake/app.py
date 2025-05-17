3ce58dd0df3d9a926f95c2558a7f24a7550a4faf
celsius narhwal <hello@celsiusnarhwal.dev>
2025-05-17 00:44:18 -0400

    Update app.py

--- snowflake/app.py
+++ snowflake/app.py
@@ -4,7 +4,8 @@ import urllib.parse
 import uuid
 
 from authlib.common.errors import AuthlibHTTPError
-from authlib.jose import JoseError
+from authlib.jose import JoseError, jwt
+from authlib.oidc.core import IDToken
 from fastapi import Depends, FastAPI, Request
 from fastapi.security import (
     HTTPAuthorizationCredentials,
@@ -78,7 +79,9 @@ async def token(
     resp = await discord.get("users/@me", token=discord_token)
     resp.raise_for_status()
 
-    return utils.create_id_token(client_id=discord.client_id, user_info=resp.json())
+    return utils.create_id_token(
+        issuer=request.base_url, client_id=discord.client_id, user_info=resp.json()
+    )
 
 
 @app.get("/userinfo")
@@ -86,9 +89,14 @@ async def user_info(
     credentials: t.Annotated[HTTPAuthorizationCredentials, Depends(HTTPBearer())],
 ):
     try:
-        return utils.decode_jwt(credentials.credentials)
+        decoded_jwt = jwt.decode(
+            credentials.credentials, utils.get_jwks(), claims_cls=IDToken
+        )
+        decoded_jwt.validate()
     except JoseError:
         raise HTTPException(401)
+
+    return decoded_jwt
 
 
 @app.get("/.well-known/jwks.json")
@@ -99,15 +107,15 @@ async def jwks():
 @app.get("/.well-known/openid-configuration")
 async def discovery(request: Request):
     return {
-        "issuer": settings().public_url,
+        "issuer": request.base_url,
         "claims_supported": [
             "sub",
             "name",
             "preferred_username",
             "locale",
+            "picture",
             "email",
             "email_verified",
-            "picture",
         ],
         "grant_types_supported": ["authorization_code"],
         "id_token_signing_alg_values_supported": ["RS256"],
@@ -116,6 +124,6 @@ async def discovery(request: Request):
         "scopes_supported": ["openid", "profile", "email"],
         "authorization_endpoint": str(request.url_for("authorize")),
         "token_endpoint": str(request.url_for("token")),
-        "userinfo_endpoint": str(request.url_for("userinfo")),
+        "userinfo_endpoint": str(request.url_for("user_info")),
         "jwks_uri": str(request.url_for("jwks")),
     }
