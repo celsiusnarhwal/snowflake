4d84e99b1eb4c41b417e24e13bd4fdd5cfe815d1
celsius narhwal <hello@celsiusnarhwal.dev>
2026-01-28 12:19:21 -0500

    Update cli.py

--- snowflake/cli.py
+++ snowflake/cli.py
@@ -6,6 +6,6 @@ from joserfc.jwk import RSAKey
 def keygen():
     key = RSAKey.generate_key(
         2048, parameters={"use": "sig", "alg": "RS256"}, private=True
-    ).as_dict(private=True)
+    )
 
-    print(json.dumps(key))
+    print(json.dumps(key.as_dict(private=True)))
